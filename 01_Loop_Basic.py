e66e077852f36a068b7891b2ea5b7e5eaeccfd89
SwathedSplash62 <138179868+SwathedSplash62@users.noreply.github.com>
2023-07-24 13:13:37 +1200

    Add files via upload

---
+++ 01_Loop_Basic.py
@@ -0,0 +1,11 @@
+Valid = False
+while not Valid:
+
+    response = float(input("Enter a number: "))
+
+    if response > 0:
+        valid = True
+
+    else:
+        print("Please enter a number that is more than zero you useless waster of time")
+        print()
