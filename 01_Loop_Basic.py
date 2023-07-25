703a7535e209d7533125d2ad6004057b1a7458e7
SwathedSplash62 <138179868+SwathedSplash62@users.noreply.github.com>
2023-07-26 11:05:25 +1200

    Add files via upload

--- 01_Loop_Basic.py
+++ 01_Loop_Basic.py
@@ -1,16 +1,39 @@
-Valid = False
-while not Valid:
-    Error = "Please enter a number that is actually a number"
-    try:
 
-        response = float(input("Enter a number: "))
+# check input is a number more than zero
+def num_check(question):
+    valid = False
+    while not valid:
+        error = "Please enter something that is actually a number"
 
-        if response  > 0:
-            valid = True
+        try:
 
-        else:
-            print("Please enter a number that is more than zero")
-            print()
-    except ValueError:
-             print(Error)
-             print()
+            #ask user to enter a number
+            response = float(input(question))
+
+            #checks number is more than zero
+            if response > 0:
+                return response
+
+            #Outputs error if input is invalid
+            else:
+                print(error)
+                print()
+
+        except ValueError:
+                 print(error)
+                 print()
+
+#Main routine goes here
+width = num_check("Width: ")
+print(width)
+height = num_check("Height: ")
+print()
+print("Width", width)
+print("Height", height)
+print()
+area = width*height
+perimeter = width*2+height*2
+print()
+print("Area: {} units".format(area))
+print("Periemter: {} units".format(perimeter))
+print()
